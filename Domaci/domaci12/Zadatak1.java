b7d02802c3da4e6d5e71020607acef270bf1300b
Milos Stamenkovic <milos_stamenkovic@outlook.com>
2023-04-29 09:51:30 +0200

    Create Zadatak1.java

---
+++ Domaci/domaci12/Zadatak1.java
@@ -0,0 +1,8 @@
+package domaci12;
+
+public class Zadatak1 {
+    public static void main(String[] args) {
+        String[] arr = {"hello", "darkness", "my", "old", "friend"};
+        System.out.println(arr[0]);
+    }
+}
