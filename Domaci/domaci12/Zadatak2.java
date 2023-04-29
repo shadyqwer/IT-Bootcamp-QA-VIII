54c9952c3e0b5b9d6feaf5cb2f240201447d564a
Milos Stamenkovic <milos_stamenkovic@outlook.com>
2023-04-29 09:51:32 +0200

    Create Zadatak2.java

---
+++ Domaci/domaci12/Zadatak2.java
@@ -0,0 +1,9 @@
+package domaci12;
+
+public class Zadatak2 {
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 2, 3, 5, 8, 13, 21};
+        int x = arr[arr.length - 1] * arr.length;
+        System.out.println(x);
+    }
+}
