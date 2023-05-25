2bc12371ed35772843b25d420aeefce46be037d9
Milos Stamenkovic <milos_stamenkovic@outlook.com>
2023-05-25 11:53:11 +0200

    domaci16

--- Domaci/domaci16/Konverter.java
+++ Domaci/domaci16/Konverter.java
@@ -36,31 +36,7 @@ public class Konverter {
         } else if (valuta == Valuta.RUB) {
             System.out.println(rsd + " RSD = " + (rsd / 1.3578) + " RUB");
         } else {
-            // do else-a moze doci samo ako nismo pokrili sve vrednosti enum-a sa if-om
             System.out.println("Invalidna valuta.");
         }
-//        switch(valuta) {
-//            case EUR:
-//                System.out.println(rsd + " RSD = " + (rsd / 117.2802) +" EUR");
-//                break;
-//            case USD:
-//                System.out.println(rsd + " RSD = " + (rsd / 108.7741) +" USD");
-//                break;
-//            case AUD:
-//                System.out.println(rsd + " RSD = " + (rsd / 71.7661) +" AUD");
-//                break;
-//            case CAD:
-//                System.out.println(rsd + " RSD = " + (rsd / 80.5662) +" CAD");
-//                break;
-//            case GBP:
-//                System.out.println(rsd + " RSD = " + (rsd / 135.2401) +" GBP");
-//                break;
-//            case RUB:
-//                System.out.println(rsd + " RSD = " + (rsd / 1.3578) +" RUB");
-//                break;
-//            default:
-//                System.out.println("Invalidna valuta.");
-//                break;
-//        }
     }
 }
